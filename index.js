8d95cc6b632b58b765047ab284d8bc51dca5530b
Rokunuzzaman Bhuiya <rokunuzzamanbhuiya@gmail.com>
2022-12-15 23:10:16 +0600

    server running

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-const title = "This is Index page";
+const express = require("express");
+const cors = require("cors");
+const app = express();
 
-console.log(title);
+const port = process.env.PORT || 9000;
+
+app.get("/", (req, res) => {
+  res.status(200).send("Hello Express! All are Okay!");
+});
+
+app.listen(port, () => {
+  console.log(`Port is running: http://localhost:${port}/`);
+});
